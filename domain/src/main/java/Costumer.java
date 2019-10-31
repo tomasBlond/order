1e3beb4ff02c15c7e9ca6975b5a0276bb5efb3a0
tomasBlond <uhu8phae>
2019-10-31 19:46:36 +0100

    Added gitignore, modules, costumer and costumer repository

---
+++ domain/src/main/java/Costumer.java
@@ -0,0 +1,15 @@
+public class Costumer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String Address;
+    private int phoneNumber;
+
+    public Costumer(String firstName, String lastName, String email, String address, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        Address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ domain/src/main/java/CostumerRepository.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CostumerRepository {
+    private List<Costumer> costumerRepository = new ArrayList<>();
+}
